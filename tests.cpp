2e088b2940deb2b0aa7256b06de8dbd43002f2f2
nico <ndeleon@itba.edu.ar>
2020-03-14 15:48:09 -0300

    Version Nico basado en el diagrama de flujo

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,23 @@
-int parseCallback(char* key, char* value, void* userData);
+#include "lib.cpp"
+
+int parseCallback(char* key, char* value, void* userData){
+
+}
+
+typedef struct {
+    // TODO: COMPLETE WITH USER STRCUCTURE FOR TESTING
+} arg_struct;
+
+int main (int argc, char *argv[]) {
+
+    arg_struct userData;
+
+    int result = parseCmdLine(argc, argv, parseCallback, &userData);
+
+    printf("Result = %d\n", result);
+    if (result == -1 )
+    {
+        printf("Parsing Error!\n");
+    }
+    return 0;
+}
